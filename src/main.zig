a485a3ea3f91e17b1f82c8050b89e3f3d8133bc6
Sharad Chand <pepsighan@sharadchand.com>
2025-06-14 14:09:43 +0530

    Extract to own function

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,20 @@
 const std = @import("std");
 
-pub fn main() !void {
-    const allocator = std.heap.page_allocator;
-
+fn readZigVersion(allocator: std.mem.Allocator) ![]u8 {
     // Read the .zigversion file
     const file = try std.fs.cwd().openFile(".zigversion", .{});
     defer file.close();
 
     // Read the entire file content
     const content = try file.readToEndAlloc(allocator, 1024);
-    defer allocator.free(content);
+    return content;
+}
 
-    std.debug.print("{s}\n", .{content});
+pub fn main() !void {
+    const allocator = std.heap.page_allocator;
+
+    const zig_version = try readZigVersion(allocator);
+    defer allocator.free(zig_version);
+
+    std.debug.print("{s}\n", .{zig_version});
 }
